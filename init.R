0b19d8547c3efa4b85a0df52277010a2b0b24c4a
qiaofengmarco <727720949@qq.com>
2022-03-28 01:31:46 +0800

    first

--- init.R
+++ init.R
@@ -1,7 +1,9 @@
-my_packages = c("rmarkdown", "shinythemes", "shiny", "ggplot2", "rgdal", "rgeos", "plyr", "maptools", "sp", "plotly", "networkD3", "circlize", "leaflet", "shinycssloaders", "ggmap", "RColorBrewer", "wordcloud", "NLP", "tm", "tidyverse", "tidytext", "tidyr", "lubridate", "MASS", "caret", "ggrepel", "gt", "htmlwidgets")
+my_packages = c("rmarkdown", "shinythemes", "shiny", "ggplot2", "plyr", "maptools", "sp", "plotly", "networkD3", "circlize", "shinycssloaders", "ggmap", "RColorBrewer", "wordcloud", "tm", "tidyverse", "tidytext", "tidyr", "lubridate", "MASS", "caret", "ggrepel", "gt", "htmlwidgets")
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
   }
 }
+install.packages("rgdal", repos="https://cloud.r-project.org/")
+install.packages("leaflet")
 invisible(sapply(my_packages, install_if_missing))
