e3f7faa76521fba05515a430da52a612bd456f89
qiaofengmarco <727720949@qq.com>
2022-03-28 00:39:02 +0800

    first

--- init.R
+++ init.R
@@ -1,9 +1,7 @@
-my_packages = c("rmarkdown", "shinythemes", "shiny", "ggplot2", "rgdal", "rgeos", "plyr", "maptools", "sp", "plotly", "networkD3", "chorddiag", "circlize", "leaflet", "shinycssloaders", "ggmap", "RColorBrewer", "wordcloud", "NLP", "tm", "tidyverse", "tidytext", "tidyr", "lubridate", "MASS", "caret", "ggrepel", "gt", "htmlwidgets")
+my_packages = c("rmarkdown", "shinythemes", "shiny", "ggplot2", "rgdal", "rgeos", "plyr", "maptools", "sp", "plotly", "networkD3", "circlize", "leaflet", "shinycssloaders", "ggmap", "RColorBrewer", "wordcloud", "NLP", "tm", "tidyverse", "tidytext", "tidyr", "lubridate", "MASS", "caret", "ggrepel", "gt", "htmlwidgets")
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
   }
 }
-install.packages("devtools")
-devtools::install_github("mattflor/chorddiag", build_vignettes = TRUE)
 invisible(sapply(my_packages, install_if_missing))
