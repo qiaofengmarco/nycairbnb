ac289608630abca58503e73c21e091d01ebc8ee8
qiaofengmarco <727720949@qq.com>
2022-03-28 01:38:19 +0800

    first

--- init.R
+++ init.R
@@ -4,6 +4,6 @@ install_if_missing = function(p) {
     install.packages(p)
   }
 }
-install.packages("rgdal", repos="https://cloud.r-project.org/")
+install.packages("rgdal", repos="http://R-Forge.R-project.org")
 install.packages("leaflet")
 invisible(sapply(my_packages, install_if_missing))
