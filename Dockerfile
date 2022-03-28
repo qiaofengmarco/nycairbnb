8c698d77e7a1adfd15fb49c9146563afe3291474
qiaofengmarco <727720949@qq.com>
2022-03-28 16:54:15 +0800

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM virtualstaticvoid/heroku-docker-r:shiny
+FROM qiaofengmarco/my_heroku_docker
 ENV PORT=8080
 CMD ["/usr/bin/R", "--no-save", "--gui-none", "-f", "/app/run.R"]
--- init.R
+++
@@ -1,9 +0,0 @@
-my_packages = c("rmarkdown", "shinythemes", "shiny", "ggplot2", "plyr", "maptools", "sp", "plotly", "networkD3", "circlize", "shinycssloaders", "ggmap", "RColorBrewer", "wordcloud", "tm", "tidyverse", "tidytext", "tidyr", "lubridate", "MASS", "caret", "ggrepel", "gt", "htmlwidgets")
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
-  }
-}
-helpers.installPackages("rgdal", "terra", "raster", "leaflet")
-
-invisible(sapply(my_packages, install_if_missing))
